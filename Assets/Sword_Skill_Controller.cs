acb1a407bf4215f2ddf01a199cd2a39627335fee
wellorbetter <1419919418@qq.com>
2024-05-18 15:33:03 +0800

    投掷剑，剑的方向会被设置成速度方向，但是卡住的图层出现了bug，待修复

--- Assets/Sword_Skill_Controller.cs
+++ Assets/Sword_Skill_Controller.cs
@@ -9,6 +9,9 @@ public class Sword_Skill_Controller : MonoBehaviour
     private CircleCollider2D cd;
     private Player player;
 
+
+    private bool canRotate = true;
+
     // 这里不能用start,rb在start里面获取不到，更新之后就是这样，有空研究研究
     private void Awake()
     {
@@ -21,5 +24,23 @@ public class Sword_Skill_Controller : MonoBehaviour
     {
         rb.velocity = _dir;
         rb.gravityScale = _gravityScale;
+    }
+
+    // 强制对象的右方向与其速度方向对齐。这意味着无论对象向哪个方向移动，它都会自动面朝该方向
+    private void Update()
+    {
+        if (canRotate)
+        {
+            transform.right = rb.velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canRotate = false;
+        cd.enabled = false;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        transform.parent = collision.transform;
     }
 }
